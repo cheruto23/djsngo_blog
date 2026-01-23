bd44a7b4e9852ed7e242d856743e5b9684c59e7f
cheruto <joyglory213@gmail.com>
2026-01-23 13:57:21 +0300

    update blog pagination

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,14 @@ from .views import (PostListView,
                     PostDetailView,
                     PostCreateView,
                     PostUpdateView,
-                    PostDeleteView
+                    PostDeleteView,
+                    UserPostListView
                     )
 
 urlpatterns= [
     # path('',views.home, name='blog-home'),
     path('',PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>/',UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/',PostDetailView.as_view(), name='blog-detail'),
     path('post/new/',PostCreateView.as_view(), name='blog-create'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(), name='blog-update'),
